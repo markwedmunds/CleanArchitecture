5092c49f5ffeb2b543a8e24efbd2a73b7e8d093f
Mark Edmunds <mark.edmunds@hybrid-designs.co.uk>
2023-10-04 18:58:11 +0100

    Initial Commit

---
+++ CleanArchitecture/CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureApp.swift
+//  CleanArchitecture
+//
+//  Created by Mark Edmunds on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
